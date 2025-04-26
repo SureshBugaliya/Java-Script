46c03ee2d4a25e13f7962df2cd813ac495805dd5
Suresh Bugaliya <sureshbugaliya877@gmail.com>
2025-04-26 05:02:22 +0000

    Variables in js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,17 @@ const accountId = 144553;
 let accountEmail = "sureshbugaliya877@gmail.com";
 var accounPassword = "123456789";
 accountCity = "jaipur";
+
+// accountId = 22; // not allowed 
+accountEmail = "suresh@gmil.com";
+accounPassword = "898888";
+accountCity = "pune";
+let accountState;
+
+// console.log(accountId);
+
+/* prefer to not used var 
+becouse is issue in block scope and funcilate scope */ 
+
+
+console.table([accountId, accountEmail, accounPassword, accountCity, accountState]);
