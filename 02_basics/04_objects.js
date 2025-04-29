db9380b92c3c10efada1e7db4ad078b633e4a44e
Suresh Bugaliya <sureshbugaliya877@gmail.com>
2025-04-29 04:08:30 +0000

    obj part 2 completed

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -51,3 +51,10 @@ const user =[
     },
 ]
 console.log(user[1].email);
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));
+
